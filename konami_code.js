f21a3d39cfa3594f8af585fa67e46bd1bda9d9ae
Gino Desangles <blushingbox@yahoo.com>
2019-03-13 02:28:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ function init() {
   let i = 0
   
   input.addEventListener('keydown', function(e) {
-    let key = e.key;
+    const key = e.key;
     if ( key === codes[i] ) {
       i++;
       if ( i === codes.length ) {
